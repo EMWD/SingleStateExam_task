7d587d2ad54c6aec20f2a1a9a66be3c8edecea60
EMWD <47781899+EMWD@users.noreply.github.com>
2019-03-23 21:42:02 +0300

    Add files via upload
    My decision

---
+++ Start1.cpp
@@ -0,0 +1,22 @@
+﻿#include "Class2.h"
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+	string s = "112";
+
+	double q = atoi(s.c_str());
+
+	cout <<  q*2.4;
+
+	return 0;
+}
+
+	
